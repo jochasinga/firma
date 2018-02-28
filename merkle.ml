182a09084fee143c282ec03e2975428266aa6af5
Joe Chasinga <jo.chasinga@gmail.com>
2018-02-27 16:26:55 -0800

    Add peek_all

--- merkle.ml
+++ merkle.ml
@@ -1,3 +1,4 @@
+open Printf
 open Cryptokit
 
 type 'a tree =
@@ -8,10 +9,12 @@ let new_tree gen_kwd =
   let hash_str = hash_string (Hash.sha2 256) gen_kwd in
   Leaf hash_str
 
-let rec fringe : 'a tree -> 'a list = function
+let rec fringe = function
   | Leaf x -> [x]
-  | Branch(left, right) -> fringe left @ fringe right
+  | Branch (left, right) -> fringe left @ fringe right
 
+let same_fringe t1 t2 = fringe t1 = fringe t2
+  
 let unfringe l =
   let rec aux l' tree =
     match l' with
@@ -20,11 +23,23 @@ let unfringe l =
     | x::xs -> aux xs (Branch (tree, Leaf x))
   in
   match l with
-  | [] -> Leaf None
+  | [] -> Leaf ""
   | y::[] -> Leaf y
   | y::ys -> aux ys (Leaf y)
 
 let append_tree tree kwd_l kwd_r =
-    let flattened = fringe tree in
-    let new_flattened = kwd_l :: kwd_r :: flattened in
-    unfringe new_flattened
+  let flattened = fringe tree in
+  let new_flattened = kwd_l :: kwd_r :: flattened in
+  unfringe new_flattened
+
+let peek_all tree =
+  let flattened = fringe tree in
+  let len = List.length flattened in
+  let rec aux l acc1 acc2 =
+    if acc1 <= 0
+    then ()
+    else
+      match l with
+      | [] -> print_newline ()
+      | x::xs -> printf "%d: %S\n" acc2 x; aux xs (acc1 - 1) (acc2 + 1)
+  in aux flattened len 0
--- merkle.mli
+++ merkle.mli
@@ -1,6 +1,7 @@
 type 'a tree
 
 val fringe : 'a tree -> 'a list
-val unfringe : 'a option list -> 'a option tree
+val unfringe : string list -> string tree
 val new_tree : string -> string tree
-val append_tree : 'a option tree -> 'a option -> 'a option -> 'a option tree
+val append_tree : string tree -> string tree -> string tree
+val peek_all : string tree -> unit
