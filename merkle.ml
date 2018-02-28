0617057bd074d2e903821322a82bd6cf81d230a1
Joe Chasinga <jo.chasinga@gmail.com>
2018-02-28 10:42:43 -0800

    Add hashpair tree

--- merkle.ml
+++ merkle.ml
@@ -1,17 +1,27 @@
 open Printf
 open Cryptokit
 
+type payload = string * string
 type 'a tree =
   | Leaf
   | Node of 'a * 'a tree * 'a tree
 
 let insert x s = Node (x, Leaf, s)
 
+let insert_hashpair = fun x s ->
+  let pair = x, hash_string (Hash.sha2 256) x in Node (pair, Leaf, s)
+
 let rec new_tree: ('a list -> 'a tree) = fun l ->
   let empty = Leaf in
   match l with
   | [] -> empty
   | x :: l' -> insert x (new_tree l')
+
+let rec new_hashtree = fun l ->
+  let empty = Leaf in
+  match l with
+  | [] -> empty
+  | x :: l' -> insert_hashpair x (new_hashtree l')
 
 let rec mem x = function
   | Leaf -> false
@@ -40,4 +50,20 @@ let peek_all: (string tree -> unit) = fun t ->
       match l with
       | [] -> print_newline ()
       | x::xs -> printf "%d: %S\n" acc2 x; aux xs (acc1 - 1) (acc2 + 1)
+  in aux flattened len 0
+
+let peek_pair_all: (payload tree -> unit) = fun t ->
+  let flattened = fringe t in
+  let len = List.length flattened in
+  let rec aux l acc1 acc2 =
+    if acc1 <= 0
+    then ()
+    else match l with
+         | [] -> print_newline ()
+         | x::xs ->
+            begin
+              let a, b = x in
+              printf "%d: (%S, %S)\n" acc2 a b;
+              aux xs (acc1 - 1) (acc2 + 1)
+            end
   in aux flattened len 0
--- merkle.mli
+++ merkle.mli
@@ -1,7 +1,11 @@
 type 'a tree = Leaf | Node of 'a * 'a tree * 'a tree
+type payload = string * string
 
 val fringe : 'a tree -> 'a list
 val new_tree : 'a list -> 'a tree
+val new_hashtree : string list -> payload tree
 val insert : 'a -> 'a tree -> 'a tree
+val insert_hashpair : string -> payload tree -> payload tree
 val mem : 'a -> 'a tree -> bool
 val peek_all : string tree -> unit
+val peek_pair_all : payload tree -> unit
